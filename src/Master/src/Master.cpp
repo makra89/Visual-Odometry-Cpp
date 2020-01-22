b0838f89957009181f9d9a90cb02ff67d3641f31
makra89 <manuel.mk.kraus@gmail.com>
2020-01-22 22:53:43 +0100

    Add frame unit tests

--- src/Master/src/Master.cpp
+++ src/Master/src/Master.cpp
@@ -18,61 +18,61 @@ namespace Master
 
 Master::Master()
 {
-	// Instantiate all feature handling members
-	m_detector = FeatureHandling::InstantiateFeatureDetector("Harris");
-	m_descriptor = FeatureHandling::InstantiateFeatureDescriptor("Brief");
-	m_matcher = FeatureHandling::InstantiateFeatureMatcher("BruteForce");
+    // Instantiate all feature handling members
+    m_detector = FeatureHandling::InstantiateFeatureDetector("Harris");
+    m_descriptor = FeatureHandling::InstantiateFeatureDescriptor("Brief");
+    m_matcher = FeatureHandling::InstantiateFeatureMatcher("BruteForce");
 
-	// Instantiate last processed frame as invalid
-	m_lastFrame = Utils::Frame();
+    // Instantiate last processed frame as invalid
+    m_lastFrame = Utils::Frame();
 }
 
 bool Master::FeedNextFrame(Utils::Frame& in_frame)
 {
-	bool ret = true;
-	
-	if (m_detector == NULL || m_descriptor == NULL || m_matcher == NULL)
-	{
-		std::cout << "[Master]: Feature handling members could not be instantiated" << std::endl;
-		ret = false;
-	}
-	else
-	{
-		// Measure frame processing time
-		cv::TickMeter tick;
-		tick.start();
+    bool ret = true;
+    
+    if (m_detector == NULL || m_descriptor == NULL || m_matcher == NULL)
+    {
+        std::cout << "[Master]: Feature handling members could not be instantiated" << std::endl;
+        ret = false;
+    }
+    else
+    {
+        // Measure frame processing time
+        cv::TickMeter tick;
+        tick.start();
 
-		// Get features and descriptions
-		ret = m_detector->ExtractKeypoints(in_frame);
-		ret = ret && m_descriptor->ComputeDescriptions(in_frame);
+        // Get features and descriptions
+        ret = m_detector->ExtractKeypoints(in_frame);
+        ret = ret && m_descriptor->ComputeDescriptions(in_frame);
 
-		tick.stop();
-		std::cout << "[Master]: Frame processing time: " << tick.getTimeMilli() << std::endl;
+        tick.stop();
+        std::cout << "[Master]: Frame processing time: " << tick.getTimeMilli() << std::endl;
 
-		if (ret && m_lastFrame.isValid())
-		{
-			// Get matches and draw them
-			ret = m_matcher->matchDesriptions(in_frame, m_lastFrame);
-			cv::Mat matchImage = in_frame.GetImageCopy(false);
+        if (ret && m_lastFrame.isValid())
+        {
+            // Get matches and draw them
+            ret = m_matcher->matchDesriptions(in_frame, m_lastFrame);
+            cv::Mat matchImage = in_frame.GetImageCopy();
 
-			for (auto matches : in_frame.GetMatches())
-			{
-				// Draw keypoints from current frame
-				cv::circle(matchImage, in_frame.GetKeypoints()[matches.queryIdx].pt, 5, cv::Scalar(0, 0.0, 255.0), 2);
-				// Draw keypoints from last frame
-				cv::circle(matchImage, m_lastFrame.GetKeypoints()[matches.trainIdx].pt, 5, cv::Scalar(0.0, 255.0, 0.0), 2);
-				// Draw connecting line
-				cv::line(matchImage, in_frame.GetKeypoints()[matches.queryIdx].pt, m_lastFrame.GetKeypoints()[matches.trainIdx].pt, cv::Scalar(0.0, 0.0, 255.0), 2);
-			}
+            for (auto matches : in_frame.GetMatches())
+            {
+                // Draw keypoints from current frame
+                cv::circle(matchImage, in_frame.GetKeypoints()[matches.queryIdx].pt, 5, cv::Scalar(0, 0.0, 255.0), 2);
+                // Draw keypoints from last frame
+                cv::circle(matchImage, m_lastFrame.GetKeypoints()[matches.trainIdx].pt, 5, cv::Scalar(0.0, 255.0, 0.0), 2);
+                // Draw connecting line
+                cv::line(matchImage, in_frame.GetKeypoints()[matches.queryIdx].pt, m_lastFrame.GetKeypoints()[matches.trainIdx].pt, cv::Scalar(0.0, 0.0, 255.0), 2);
+            }
 
-			cv::imshow("Optical Flow", matchImage);
-			cv::waitKey(20);
-		}
+            cv::imshow("Optical Flow", matchImage);
+            cv::waitKey(20);
+        }
 
-		m_lastFrame = in_frame;
-	}
+        m_lastFrame = in_frame;
+    }
 
-	return ret;
+    return ret;
 }
 
 } //namespace Master
