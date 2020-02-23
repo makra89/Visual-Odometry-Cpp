2f21e543123970a6b66b3f4bc0eccd35bad7d673
makra89 <manuel.mk.kraus@gmail.com>
2020-02-23 01:07:30 +0100

    Try 1

--- tools/build/windows_latest_ci.bat
+++ tools/build/windows_latest_ci.bat
@@ -1,1 +1,5 @@
-setx path "D:\a\Visual-Odometry-Cpp\Visual-Odometry-Cpp\opencv-install\x64\vc16\bin;%PATH%"
+SET PATH=D:\a\Visual-Odometry-Cpp\Visual-Odometry-Cpp\opencv-install\x64\vc16\bin;%PATH%
+
+D:\a\Visual-Odometry-Cpp\Visual-Odometry-Cpp\src\DeltaPoseReconstruction\test\Release\Vocpp_DeltaPoseReconstructionTest.exe
+D:\a\Visual-Odometry-Cpp\Visual-Odometry-Cpp\src\Interface\test\Release\Vocpp_InterfaceTest.exe
+D:\a\Visual-Odometry-Cpp\Visual-Odometry-Cpp\src\Utils\test\Release\Vocpp_UtilsTest.exe
