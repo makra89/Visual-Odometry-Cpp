eabcd5601cf77d8050288072f507b28ca9a4587c
Manuel Kraus <58955879+makra89@users.noreply.github.com>
2021-02-05 23:07:26 +0100

    Update RansacTest.cpp

--- src/DeltaPoseReconstruction/test/src/RansacTest.cpp
+++ src/DeltaPoseReconstruction/test/src/RansacTest.cpp
@@ -339,15 +339,15 @@ TEST(RansacTest, PureTranslation_WithOutliers)
         // We might loose some inliers
         EXPECT_GE(inlierIndices.size(), 90);
 
-        // Should be very close to zero, but due to the outliers it might be that the "Full" model is picked
-        EXPECT_NEAR(1.F, rotation(0, 0), 1e-5);
-        EXPECT_NEAR(0.F, rotation(0, 1), 1e-5);
-        EXPECT_NEAR(0.F, rotation(0, 2), 1e-5);
-        EXPECT_NEAR(0.F, rotation(1, 0), 1e-5);
-        EXPECT_NEAR(1.F, rotation(1, 1), 1e-5);
-        EXPECT_NEAR(0.F, rotation(1, 2), 1e-5);
-        EXPECT_NEAR(0.F, rotation(2, 1), 1e-5);
-        EXPECT_NEAR(1.F, rotation(2, 2), 1e-5);
+        // Should be very close to true one, but due to the outliers it might be that the "Full" model is picked
+        EXPECT_NEAR(1.F, rotation(0, 0), 1e-4);
+        EXPECT_NEAR(0.F, rotation(0, 1), 1e-4);
+        EXPECT_NEAR(0.F, rotation(0, 2), 1e-4);
+        EXPECT_NEAR(0.F, rotation(1, 0), 1e-4);
+        EXPECT_NEAR(1.F, rotation(1, 1), 1e-4);
+        EXPECT_NEAR(0.F, rotation(1, 2), 1e-4);
+        EXPECT_NEAR(0.F, rotation(2, 1), 1e-4);
+        EXPECT_NEAR(1.F, rotation(2, 2), 1e-4);
 
         // Translation is only known up to a global scale
         translationTrue = translationTrue / cv::norm(translationTrue);
